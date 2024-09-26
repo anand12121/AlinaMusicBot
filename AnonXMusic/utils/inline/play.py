a547bd5549d14a3d56fa20ed473deb0adbb4d621
Payal8800 <124197031+Payal8800@users.noreply.github.com>
2024-09-26 13:05:48 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -54,17 +54,18 @@ def stream_markup_timer(_, chat_id, played, dur):
         bar = "—————————◉"
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
+        ],
+        [
+            InlineKeyboardButton(
+                text="✨ ​🇸​​🇺​​🇵​​🇵​​🇴​​🇷​​🇹​ ✨", url=f"https://t.me/Dilll_se"
+            ),
+            InlineKeyboardButton(
+                text="💫 सहायक समूह 💫", url=f"https://t.me/starrrkinngg"
+          ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -74,11 +75,12 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(
+                text="✨ सेवक ✨", url=f"https://t.me/Ishq_Sukoon"
+            ),
+            InlineKeyboardButton(
+                text="💫 सहायक समूह 💫", url=f"https://t.me/ChhotiSiDuniya"
+          ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
